e8ae8e26d53f2d75f4aad57c67d06d9983ff8203
david <davidlimjh77@gmail.com>
2021-10-11 20:12:39 +0800

    basic & comfortable working

---
+++ globals.js
@@ -0,0 +1,23 @@
+// Please implement exercise logic here
+// keep data about the game in a 2-D array
+//only board with odd numbers can be created
+let boardSize = 3
+
+let board = []
+
+// the element that contains the rows and squares
+let boardElement;
+
+// the element that contains the entire board
+// we can empty it out for convenience
+let boardContainer;
+
+// current player global starts at X
+let currentPlayer = "X";
+
+let win = false
+
+let outputElement
+
+let boardSizeInput
+let submitButton
