ae10234134d131790bfad37906ca75026d5efde5
Goh Yong Siang <gysiang@gmail.com>
2021-12-02 22:06:15 +0800

    working version of tic tac toe with reset function and win logic.

--- script.js
+++ script.js
@@ -1,1 +1,158 @@
 // Please implement exercise logic here
+// global variables
+// keep data about the game in a 2-D array
+let board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
+
+// the element that contains the rows and squares
+let boardElement;
+
+// the element that contains the entire board
+// we can empty it out for convenience
+let boardContainer;
+
+// current player global starts at X
+let currentPlayer = 'X';
+
+// create a div element to display a message
+const messageDiv = document.createElement('div');
+messageDiv.classList.add('outputBox');
+
+// helper function to display message
+const output = (message) => {
+  messageDiv.innerText = message;
+};
+
+// switch the global values from one player to the next
+const togglePlayer = () => {
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O';
+  } else {
+    currentPlayer = 'X';
+  }
+};
+
+// create the winning logic for this game
+// to win the player has get x or o in a line
+const checkWin = (board) => {
+  // check every position
+  // there is a conditional for all 15 win conditions
+  if ((board[0][0] !== '') && (board[0][0] === board[0][1] && board[0][1] === board[0][2])) {
+    return true;
+  }
+  if ((board[1][0] !== '') && (board[1][0] === board[1][1] && board[1][1] === board[1][2])) {
+    return true;
+  }
+  if ((board[2][0] !== '') && (board[2][0] === board[1][2] && board[1][2] === board[2][2])) {
+    return true;
+  }
+  if ((board[0][0] !== '') && (board[0][0] === board[1][0] && board[1][0] === board[2][0])) {
+    return true;
+  }
+  if ((board[0][1] !== '') && (board[0][1] === board[1][1] && board[1][1] === board[2][1])) {
+    return true;
+  }
+  if ((board[0][2] !== '') && (board[0][2] === board[1][2] && board[1][2] === board[2][2])) {
+    console.log('Row 3');
+    return true;
+  }
+  if ((board[2][0] !== '') && (board[2][0] === board[2][1]) && board[2][1] === board[2][2]) {
+    return true;
+  }
+
+  if ((board[0][0] !== '') && (board[0][0] === board[1][1] && board[1][1] === board[2][2])) {
+    return true;
+  }
+  if ((board[0][2] !== '') && (board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
+    return true;
+  }
+  console.log('row 3 column 1');
+  console.log(board[0][2]);
+  console.log(board[1][2]);
+  console.log(board[2][2]);
+  return false;
+};
+
+// completely rebuilds the entire board every time there's a click
+const buildBoard = () => {
+  // start with an empty container
+  boardContainer.innerHTML = '';
+  boardElement = document.createElement('div');
+  boardElement.classList.add('board');
+
+  // move through the board data array and create the
+  // current state of the board
+  for (let i = 0; i < board.length; i += 1) {
+    // separate var for one row / row element
+    const row = board[i];
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+
+    // set each square
+    // j is the column number
+    for (let j = 0; j < row.length; j += 1) {
+      // one square element
+      const square = document.createElement('div');
+      square.classList.add('square');
+
+      // set the text of the square according to the array
+      square.innerText = board[i][j];
+
+      rowElement.appendChild(square);
+
+      // set the click all over again
+      // eslint-disable-next-line
+      square.addEventListener("click", () => {
+        // eslint-disable-next-line no-use-before-define
+        squareClick(i, j);
+      });
+    }
+
+    // add a single row to the board
+    boardContainer.appendChild(rowElement);
+  }
+};
+
+const squareClick = (column, row) => {
+  // if the square user clicks is blank, check if the user wins the
+  // game, if not toggle next player.
+  if (board[column][row] === '') {
+    board[column][row] = currentPlayer;
+    buildBoard(board);
+
+    if (checkWin(board) === true) {
+      output(`Player ${currentPlayer} wins!`);
+      // empty the board after a set time
+      setTimeout(() => {
+        board = [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ];
+        // rebuild the board and assign default player back to 1
+        buildBoard(board);
+        currentPlayer = 'X';
+        output('Please click on any square!');
+      }, 3000);
+    } else {
+      togglePlayer();
+    }
+  }
+};
+
+// create the board container element and put it on the screen
+const initGame = () => {
+  boardContainer = document.createElement('div');
+  document.body.appendChild(boardContainer);
+
+  // build the board - right now it's empty
+  buildBoard(board);
+
+  // insert the output div
+  document.body.appendChild(messageDiv);
+};
+
+initGame();
