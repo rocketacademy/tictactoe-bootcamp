a0a5270c26c9fbead6062f209c45000478103f01
supershazwi <supershazwi@gmail.com>
2021-12-04 09:01:14 +0800

    multifactor

--- script.js
+++ script.js
@@ -6,26 +6,6 @@ const board = [
   ["", "", ""],
 ];
 
-const winningNumbers = [
-  ["00", "01", "02"],
-  ["10", "11", "12"],
-  ["20", "21", "22"],
-  ["00", "10", "20"],
-  ["01", "11", "21"],
-  ["02", "12", "22"],
-  ["00", "11", "22"],
-  ["20", "11", "02"],
-];
-
-// if any of this 0 hit 3 first, the player wins, if not, draw
-
-const playerCheck = {
-  X: [0, 0, 0, 0, 0, 0, 0, 0],
-  O: [0, 0, 0, 0, 0, 0, 0, 0],
-};
-
-let roundWon = false;
-
 // the element that contains the rows and squares
 let boardElement;
 
@@ -109,26 +89,6 @@ const squareClick = (row, column) => {
 
       // change the player
       togglePlayer();
-    }
-  }
-};
-
-const checkWinner = (row, column) => {
-  // loop through winningNumbers
-  const coordinate = `${row}${column}`;
-
-  for (let i = 0; i < winningNumbers.length; i++) {
-    if (winningNumbers[i].indexOf(coordinate) !== -1) {
-      playerCheck[currentPlayer][i]++;
-    }
-  }
-
-  for (let i = 0; i < playerCheck[currentPlayer].length; i++) {
-    if (playerCheck[currentPlayer][i] == 3) {
-      document.getElementById(
-        "message"
-      ).innerHTML = `Player ${currentPlayer} wins!`;
-      roundWon = true;
     }
   }
 };
