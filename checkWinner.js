71c0d02760459c861cf87090fbcfa7ce7b8bb031
supershazwi <supershazwi@gmail.com>
2021-12-04 15:51:50 +0800

    update

--- checkWinner.js
+++ checkWinner.js
@@ -1,45 +1,45 @@
-const winningNumbers = [
-  ["00", "01", "02"],
-  ["10", "11", "12"],
-  ["20", "21", "22"],
-  ["00", "10", "20"],
-  ["01", "11", "21"],
-  ["02", "12", "22"],
-  ["00", "11", "22"],
-  ["20", "11", "02"],
-];
+// const winningNumbers = [
+//   ["00", "01", "02"],
+//   ["10", "11", "12"],
+//   ["20", "21", "22"],
+//   ["00", "10", "20"],
+//   ["01", "11", "21"],
+//   ["02", "12", "22"],
+//   ["00", "11", "22"], // diagonal
+//   ["20", "11", "02"], // diagonal
+// ];
 
-// if any of this 0 hit 3 first, the player wins, if not, draw
+// // if any of this 0 hit 3 first, the player wins, if not, draw
 
-const playerCheck = {
-  X: [0, 0, 0, 0, 0, 0, 0, 0],
-  O: [0, 0, 0, 0, 0, 0, 0, 0],
-};
+// const playerCheck = {
+//   X: [0, 0, 0, 0, 0, 0, 0, 0],
+//   O: [0, 0, 0, 0, 0, 0, 0, 0],
+// };
 
-let roundWon = false;
-let squareCount = 0;
+// let roundWon = false;
+// let squareCount = 0;
 
-const checkWinner = (row, column) => {
-  // loop through winningNumbers
-  const coordinate = `${row}${column}`;
+// const checkWinner = (row, column) => {
+//   // loop through winningNumbers
+//   const coordinate = `${row}${column}`;
 
-  for (let i = 0; i < winningNumbers.length; i++) {
-    if (winningNumbers[i].indexOf(coordinate) !== -1) {
-      playerCheck[currentPlayer][i]++;
-    }
-  }
+//   for (let i = 0; i < winningNumbers.length; i++) {
+//     if (winningNumbers[i].indexOf(coordinate) !== -1) {
+//       playerCheck[currentPlayer][i]++;
+//     }
+//   }
 
-  for (let i = 0; i < playerCheck[currentPlayer].length; i++) {
-    if (playerCheck[currentPlayer][i] == 3) {
-      document.getElementById(
-        "message"
-      ).innerHTML = `Player ${currentPlayer} wins!`;
-      roundWon = true;
-    }
-  }
+//   for (let i = 0; i < playerCheck[currentPlayer].length; i++) {
+//     if (playerCheck[currentPlayer][i] == 3) {
+//       document.getElementById(
+//         "message"
+//       ).innerHTML = `Player ${currentPlayer} wins!`;
+//       roundWon = true;
+//     }
+//   }
 
-  squareCount++;
-  if (squareCount == 9 && roundWon == false) {
-    document.getElementById("message").innerHTML = `Draw!`;
-  }
-};
+//   squareCount++;
+//   if (squareCount == 9 && roundWon == false) {
+//     document.getElementById("message").innerHTML = `Draw!`;
+//   }
+// };
