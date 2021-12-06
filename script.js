5ef95f5910ce8a73a3bd343e133f034f02a00fdf
supershazwi <supershazwi@gmail.com>
2021-12-06 12:47:20 +0800

    completed more comfortable variable board length and win size

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
-// Please implement exercise logic here
-// keep data about the game in a 2-D array
-const board = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
-
-// the element that contains the rows and squares
+const board = [];
+let boardContainer = document.getElementById("ticTacToe");
 let boardElement;
+let boardLength = 0;
 
-// the element that contains the entire board
-// we can empty it out for convenience
-let boardContainer;
+let roundWon = false;
 
-// current player global starts at X
+let leftSquareCount;
+let rightSquareCount;
+let topSquareCount;
+let bottomSquareCount;
+
+let verticalCount;
+let horizontalCount;
+let topLeftToBottomRightCount;
+let bottomLeftToTopRightCount;
+
+let squareWinCount = 0;
+
 let currentPlayer = "X";
 
-// completely rebuilds the entire board every time there's a click
-const buildBoard = (board) => {
+document.getElementById("boardLengthInput").addEventListener("keyup", () => {
+  let boardLengthInput = parseInt(
+    document.getElementById("boardLengthInput").value
+  );
+  let noOfSquaresInput = parseInt(
+    document.getElementById("noOfSquaresInput").value
+  );
+
+  if (
+    boardLengthInput.length !== 0 &&
+    noOfSquaresInput.length !== 0 &&
+    noOfSquaresInput <= boardLengthInput
+  ) {
+    document.getElementById("start").disabled = false;
+  } else {
+    document.getElementById("start").disabled = true;
+  }
+});
+
+document.getElementById("noOfSquaresInput").addEventListener("keyup", () => {
+  let boardLengthInput = parseInt(
+    document.getElementById("boardLengthInput").value
+  );
+  let noOfSquaresInput = parseInt(
+    document.getElementById("noOfSquaresInput").value
+  );
+
+  if (
+    boardLengthInput.length !== 0 &&
+    noOfSquaresInput.length !== 0 &&
+    noOfSquaresInput <= boardLengthInput
+  ) {
+    document.getElementById("start").disabled = false;
+  } else {
+    document.getElementById("start").disabled = true;
+  }
+});
+
+document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("userInput").style.display = "none";
+  document.getElementById("ticTacToe").style.display = "";
+
+  boardLength = document.getElementById("boardLengthInput").value;
+
+  buildBoard(boardLength);
+  squareWinCount = parseInt(document.getElementById("noOfSquaresInput").value);
+});
+
+const buildBoard = (boardLength) => {
   // start with an empty container
   boardContainer.innerHTML = "";
   boardElement = document.createElement("div");
@@ -25,64 +75,58 @@ const buildBoard = (board) => {
 
   // move through the board data array and create the
   // current state of the board
-  for (let i = 0; i < board.length; i += 1) {
+  for (let i = 0; i < boardLength; i += 1) {
     // separate var for one row / row element
-    const row = board[i];
     const rowElement = document.createElement("div");
     rowElement.classList.add("row");
+    rowElement.classList.add("flex");
+
+    let boardRow = [];
 
     // set each square
     // j is the column number
-    for (let j = 0; j < row.length; j += 1) {
+    for (let j = 0; j < boardLength; j += 1) {
       // one square element
       const square = document.createElement("div");
       square.classList.add("square");
+      square.classList.add("marker");
 
       // set the text of the square according to the array
-      square.innerText = board[i][j];
+      square.innerHTML = "";
+      boardRow.push("");
 
       rowElement.appendChild(square);
 
       // set the click all over again
       // eslint-disable-next-line
-      square.addEventListener("click", () => {
-        squareClick(i, j);
+      square.addEventListener("click", (event) => {
+        squareClick(event, i, j);
       });
     }
 
     // add a single row to the board
     boardContainer.appendChild(rowElement);
+    board.push(boardRow);
   }
 };
 
-// create the board container element and put it on the screen
-const initGame = () => {
-  boardContainer = document.createElement("div");
-  document.body.appendChild(boardContainer);
-
-  // build the board - right now it's empty
-  buildBoard(board);
-};
-
-// switch the global values from one player to the next
-const togglePlayer = () => {
-  if (currentPlayer === "X") {
-    currentPlayer = "O";
-  } else {
-    currentPlayer = "X";
-  }
-};
-
-const squareClick = (row, column) => {
+const squareClick = (event, row, column) => {
   // see if the clicked square has been clicked on before
   if (!roundWon) {
     if (board[row][column] === "") {
       // alter the data array, set it to the current player
       board[row][column] = currentPlayer;
 
-      // refresh the creen with a new board
+      // refresh the screen with a new board
       // according to the array that was just changed
-      buildBoard(board);
+      // buildBoard(board);
+      event.target.innerText = currentPlayer;
+
+      if (currentPlayer === "X") {
+        event.target.classList.add("red");
+      } else {
+        event.target.classList.add("green");
+      }
 
       // check winner
       checkWinner(row, column);
@@ -93,4 +137,240 @@ const squareClick = (row, column) => {
   }
 };
 
-initGame();
+const togglePlayer = () => {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+};
+
+// check up and down to find matches
+const checkVertical = (row, column, counter) => {
+  let currentForwardSquare;
+  let currentReverseSquare;
+
+  let previousForwardSquare;
+  let previousReverseSquare;
+
+  let startForwardRow = row + counter;
+  let startReverseRow = row - counter;
+
+  if (board[startForwardRow] !== undefined) {
+    previousForwardSquare = board[startForwardRow][column];
+    row = startForwardRow + 1;
+
+    if (row < boardLength && board[row] !== undefined) {
+      currentForwardSquare = board[row][column];
+
+      if (
+        currentForwardSquare == previousForwardSquare &&
+        currentForwardSquare !== ""
+      ) {
+        verticalCount++;
+
+        if (verticalCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+
+  if (board[startReverseRow] !== undefined) {
+    previousReverseSquare = board[startReverseRow][column];
+    row = startReverseRow - 1;
+
+    if (row >= 0 && board[row] !== undefined) {
+      currentReverseSquare = board[row][column];
+
+      if (
+        currentReverseSquare == previousReverseSquare &&
+        currentReverseSquare !== ""
+      ) {
+        verticalCount++;
+
+        if (verticalCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+};
+
+// check left and right to find matches
+const checkHorizontal = (row, column, counter) => {
+  let currentForwardSquare;
+  let currentReverseSquare;
+
+  let previousForwardSquare;
+  let previousReverseSquare;
+
+  let startForwardColumn = column + counter;
+  let startReverseColumn = column - counter;
+
+  previousForwardSquare = board[row][startForwardColumn];
+  column = startForwardColumn + 1;
+
+  if (column < boardLength) {
+    currentForwardSquare = board[row][column];
+
+    if (
+      currentForwardSquare == previousForwardSquare &&
+      currentForwardSquare !== ""
+    ) {
+      horizontalCount++;
+
+      if (horizontalCount == squareWinCount) {
+        roundWon = true;
+      }
+    }
+  }
+
+  previousReverseSquare = board[row][startReverseColumn];
+  column = startReverseColumn - 1;
+
+  if (column >= 0) {
+    currentReverseSquare = board[row][column];
+
+    if (
+      currentReverseSquare == previousReverseSquare &&
+      currentReverseSquare !== ""
+    ) {
+      horizontalCount++;
+
+      if (horizontalCount == squareWinCount) {
+        roundWon = true;
+      }
+    }
+  }
+};
+
+// check from top left to bottom right to find matches
+const checkDiagonalTopLeftToBottomRight = (row, column, counter) => {
+  let currentForwardSquare;
+  let currentReverseSquare;
+
+  let previousForwardSquare;
+  let previousReverseSquare;
+
+  let startForwardRow = row + counter;
+  let startForwardColumn = column + counter;
+
+  let startReverseRow = row - counter;
+  let startReverseColumn = column - counter;
+
+  if (board[startForwardRow] !== undefined) {
+    previousForwardSquare = board[startForwardRow][startForwardColumn];
+    row = startForwardRow + 1;
+    column = startForwardColumn + 1;
+
+    if (row < boardLength && column < boardLength && board[row] !== undefined) {
+      currentForwardSquare = board[row][column];
+
+      if (
+        currentForwardSquare == previousForwardSquare &&
+        currentForwardSquare !== ""
+      ) {
+        topLeftToBottomRightCount++;
+
+        if (topLeftToBottomRightCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+
+  if (board[startReverseRow] !== undefined) {
+    previousReverseSquare = board[startReverseRow][startReverseColumn];
+    row = startReverseRow + 1;
+    column = startReverseColumn + 1;
+
+    if (row < boardLength && column < boardLength && board[row] !== undefined) {
+      currentReverseSquare = board[row][column];
+
+      if (
+        currentReverseSquare == previousReverseSquare &&
+        currentReverseSquare !== ""
+      ) {
+        topLeftToBottomRightCount++;
+
+        if (topLeftToBottomRightCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+};
+
+// check from bottom left to top right to find matches
+const checkDiagonalBottomLeftToTopRight = (row, column, counter) => {
+  let currentForwardSquare;
+  let currentReverseSquare;
+
+  let previousForwardSquare;
+  let previousReverseSquare;
+
+  let startForwardRow = row - counter;
+  let startForwardColumn = column + counter;
+
+  let startReverseRow = row + counter;
+  let startReverseColumn = column - counter;
+
+  if (board[startForwardRow] !== undefined) {
+    previousForwardSquare = board[startForwardRow][startForwardColumn];
+    row = startForwardRow - 1;
+    column = startForwardColumn + 1;
+
+    if (row < boardLength && column < boardLength && board[row] !== undefined) {
+      currentForwardSquare = board[row][column];
+
+      if (
+        currentForwardSquare == previousForwardSquare &&
+        currentForwardSquare !== ""
+      ) {
+        bottomLeftToTopRightCount++;
+
+        if (bottomLeftToTopRightCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+
+  if (board[startReverseRow] !== undefined) {
+    previousReverseSquare = board[startReverseRow][startReverseColumn];
+    row = startReverseRow + 1;
+    column = startReverseColumn - 1;
+
+    if (row < boardLength && column < boardLength && board[row] !== undefined) {
+      currentReverseSquare = board[row][column];
+
+      if (
+        currentReverseSquare == previousReverseSquare &&
+        currentReverseSquare !== ""
+      ) {
+        bottomLeftToTopRightCount++;
+
+        if (bottomLeftToTopRightCount == squareWinCount) {
+          roundWon = true;
+        }
+      }
+    }
+  }
+};
+
+const checkWinner = (row, column) => {
+  // everytime user clicks a box, check 360 degrees
+  // for all boxes up till squareWinCount to find matches
+  topLeftToBottomRightCount = 1;
+  bottomLeftToTopRightCount = 1;
+  verticalCount = 1;
+  horizontalCount = 1;
+
+  for (let counter = 0; counter < squareWinCount; counter++) {
+    checkVertical(row, column, counter);
+    checkHorizontal(row, column, counter);
+    checkDiagonalTopLeftToBottomRight(row, column, counter);
+    checkDiagonalBottomLeftToTopRight(row, column, counter);
+  }
+};
