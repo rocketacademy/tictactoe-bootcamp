11fdf84ad284c73d981641963a97a2e8b95e291b
wangzaa <aikkoku@yahoo.com>
2022-01-18 11:47:19 +0800

    updated for flexible grid size

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 //////////GLOBAL VARIABLES//////////
-// keep data about the game in a 2-D array
-const board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-];
+  
+let board;
+const createBoard = (x,y)=>{
+board = [];
+  for (i=0; i<y; i +=1){
+    board.push([]);
+    for(j=0; j<x;j +=1){
+    board[i].push(''); 
+    }
+  }
+};
+createBoard(4,4);
 
-// the element that contains the rows and squares
 let boardElement;
+
 
 // the element that contains the entire board
 let boardContainer;
@@ -35,6 +41,7 @@ resetButton.addEventListener('click', ()=>{window.location.reload()})
 
 //////////HELPER FUNCTIONS//////////
 // completely rebuilds the entire board every time there's a click
+
 const buildBoard = (board) => {
   boardContainer.innerHTML = '';
   boardElement = document.createElement('div');
@@ -87,19 +94,23 @@ const squareClick = (column, row) => {
   if (board[column][row] === '') {
     // alter the data array, set it to the current player
     board[column][row] = currentPlayer;
-  
+  }
     //check winner at every turn
   if (checkWinner(board, currentPlayer) === true) {
-        messageOutput.innerText = `${currentPlayer} wins!`
+        messageOutput.innerText = `${currentPlayer} wins!!!`
         canClick = false
-      } else {
-        togglePlayer();    
-        buildBoard(board)
+      } else {  
+        togglePlayer();  
       }
-  }
+        buildBoard(board); //take this out of the if statement
 };
 
 //check winner
+// if value of board elements in a row/column/diagonal are equal to each other, win.
+// code for row: board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2]
+// code for column: board[j][i] == board[j+1][i] board[j][i] == board[j+2][i]
+// code for diagonal: board[0][0] == board[1][1] && board [0][0] == board[2][2]
+
 const checkWinner = () => { 
  for (let i = 0; i < board.length; i += 1) {
     // check rows
@@ -114,7 +125,7 @@ const checkWinner = () => {
         oCount += 1;
       }
     }
-    if (xCount === 3 || oCount === 3) {
+    if (xCount === board.length || oCount === board.length) {
       return true;
     }
 
@@ -130,11 +141,11 @@ const checkWinner = () => {
         oCount += 1;
       }
     }
-    if (xCount === 3 || oCount === 3) {
+    if (xCount === board.length || oCount === board.length) {
       return true;
     }
  }
-  //check diagonal match
+
   let diagSet = []
   let diagSetOpp = []
   for (let i = 0; i < board.length; i +=1){
@@ -151,17 +162,48 @@ const checkWinner = () => {
     return true
   }
   return false   
-  
 };
+  
+//   // check diagonals: top left to bottom right
+//   xCount = 0;
+//   oCount = 0;
+
+//   for (let j = 0; j < board.length; j += 1) {
+//     if (board[j][j] === 'X') {
+//       xCount += 1;
+//     }
+//     if (board[j][j] === 'O') {
+//       oCount += 1;
+//     }
+//      if (xCount === 3 || oCount === 3) {
+//       return true;
+//     }
+//   }
+//     // check diagonals: top right to bottom left
+//   xCount = 0;
+//   oCount = 0;
+
+//   for (let j = 0; j < board.length; j += 1) {
+//     if (board[j][board.length - 1 - j] === 'X') {
+//       xCount += 1;
+//     }
+//     if (board[j][board.length - 1 - j] === 'O') {
+//       oCount += 1;
+//     }
+//     if (xCount === 3 || oCount === 3) {
+//       return true;
+//     }
+//   }
+// };
 
 // //check row match
 //   for (let i = 0; i < board.length; i += 1) {
+//   set comes back with unique array
 //     let rowSet = new Set(board[i])
 //     if (rowSet.has("") === false && rowSet.size === 1){
 //       return true
 //     }
 //   }
-  
 // // check column match
 //   for (let i = 0; i <board.length; i+=1){
 //     let column = []
@@ -177,6 +219,8 @@ const checkWinner = () => {
 //     console.log(currentPlayer)
 //     console.log(columnSet)   
 //   }
+//check diagonal match
+ 
  
 
 ///////////////INITISATION///////////////
