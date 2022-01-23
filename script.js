417a251ec37d2bc8735aea8f32fa58a54a359b00
tabbiho <tabbiho@gmail.com>
2022-01-23 23:54:46 +0800

    User Choice Win completed

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let outputBox;
 let gameOver = false;
 let boardSize;
 let sizeBox;
+let squaresToWin;
+
+let sizeInput;
+let winningSquares;
+
+let errorText;
 
 // keep data about the game in a 2-D array
 let board;
@@ -88,29 +94,45 @@ const initGame = () => {
   buildBoard(board);
 };
 
-const sizeCheck = (num) => {
-  if (Number(num) >= 3) {
-    boardSize = Number(num);
+const sizeCheck = (num1, num2) => {
+  if (Number(num1) >= 3 && Number(num2) <= Number(num1) && Number(num2) >= 1) {
+    boardSize = Number(num1);
+    squaresToWin = Number(num2);
     sizeBox.remove();
     initGame();
+  } else {
+    errorText.innerText = 'Invalid input';
+    sizeInput.value = '';
+    winningSquares.value = '';
   }
 };
 
 const preGame = () => {
   sizeBox = document.createElement('div');
-  const sizeInput = document.createElement('input');
+  sizeInput = document.createElement('input');
   sizeInput.placeholder = 'Enter size of board (at least 3)';
-  sizeInput.style = 'width: 35%;';
+  sizeInput.className = 'input';
+
+  winningSquares = document.createElement('input');
+  winningSquares.placeholder = 'Enter number of squares to win (at least 2, not more than board size)';
+  winningSquares.className = 'input';
+
   const sizeButton = document.createElement('button');
-  sizeButton.addEventListener('click', () => sizeCheck(sizeInput.value));
+  sizeButton.className = 'button';
+  sizeButton.addEventListener('click', () => sizeCheck(sizeInput.value, winningSquares.value));
   sizeButton.innerText = 'Submit';
+
+  errorText = document.createElement('div');
+  errorText.style = 'margin-top: 10px;';
+
   sizeBox.appendChild(sizeInput);
+  sizeBox.appendChild(winningSquares);
   sizeBox.appendChild(sizeButton);
+  sizeBox.appendChild(errorText);
 
   document.body.appendChild(sizeBox);
 };
 preGame();
-// initGame();
 
 // switch the global values from one player to the next
 const togglePlayer = () => {
@@ -125,9 +147,9 @@ const checkEqual = (array) => new Set(array).size === 1;
 
 const inverseBoard = (brd) => {
   const inverse = [];
-  for (let i = 0; i < boardSize; i += 1) {
+  for (let i = 0; i < squaresToWin; i += 1) {
     inverse.push([]);
-    for (let j = 0; j < boardSize; j += 1) {
+    for (let j = 0; j < squaresToWin; j += 1) {
       inverse[i].push(brd[j][i]);
     }
   }
@@ -138,9 +160,9 @@ const diagonalBoard = (brd) => {
   const diagonal = [];
   for (let i = 0; i < 2; i += 1) {
     diagonal.push([]);
-    for (let j = 0; j < boardSize; j += 1) {
+    for (let j = 0; j < squaresToWin; j += 1) {
       const k = j;
-      const l = (boardSize - 1) - k;
+      const l = (squaresToWin - 1) - k;
       if (i === 0) {
         diagonal[i].push(brd[j][k]);
       }
@@ -152,28 +174,47 @@ const diagonalBoard = (brd) => {
   return diagonal;
 };
 
+const newBoardEvalSet = (a, b) => {
+  const newSet = [];
+  let row = a;
+  let column = b;
+  for (let i = 0; i < squaresToWin; i += 1) {
+    newSet.push([]);
+    column = b;
+    for (let j = 0; j < squaresToWin; j += 1) {
+      newSet[i].push(board[row][column]);
+      column += 1;
+    }
+    row += 1;
+  }
+  return newSet;
+};
+
 const checkWin = (brd) => {
   let win = false;
-  const inverse = inverseBoard(brd);
-  const diagonal = diagonalBoard(brd);
+  for (let i = 0; i < (brd.length + 1 - squaresToWin); i += 1) {
+    for (let j = 0; j < (brd.length + 1 - squaresToWin); j += 1) {
+      const evalSet = newBoardEvalSet(i, j);
+      const inverse = inverseBoard(evalSet);
+      const diagonal = diagonalBoard(evalSet);
 
-  for (let i = 0; i < brd.length; i += 1) {
-    // checking horizontal
-    if (checkEqual(brd[i]) && !brd[i].includes('')) {
-      win = true;
-      console.log('horizontal match!');
-    }
-    // checking vertical
-    if (checkEqual(inverse[i]) && !inverse[i].includes('')) {
-      win = true;
-      console.log('vertical match!');
-    }
-    // checking diagonal
-    if (checkEqual(diagonal[i]) && !diagonal[i].includes('')) {
-      win = true;
-      console.log('diagonal match!');
-    }
-  }
+      for (let k = 0; k < evalSet.length; k += 1) {
+        // checking horizontal
+        if (checkEqual(evalSet[k]) && !evalSet[k].includes('')) {
+          win = true;
+          console.log('horizontal match!');
+        }
+        // checking vertical
+        if (checkEqual(inverse[k]) && !inverse[k].includes('')) {
+          win = true;
+          console.log('vertical match!');
+        }
+        // checking diagonal
+        if (checkEqual(diagonal[k]) && !diagonal[k].includes('')) {
+          win = true;
+          console.log('diagonal match!');
+        }
+      } } }
   return win;
 };
 
